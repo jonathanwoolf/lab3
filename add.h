308c61bd089140df6a2bdc08cca94980abd7d3a5
jonathan woolf <jwool003@ucr.edu>
2017-04-19 18:57:33 -0700

    add header is here

---
+++ add.h
@@ -0,0 +1,21 @@
+#ifndef __ADD_H__
+#define __ADD_H__
+
+#include "base.h"
+
+class Add: public Base
+{
+public: 
+char x;
+Base* childLeft;
+Base* childRight;
+
+//Constructors
+Add(); 
+Add(Base*, Base*);
+
+//Virtual Functions
+virtual double evaluate();
+};
+
+#endif
